ec6cc7b9963d75547d47a7a88e99c891d430e7fa
TANIGUCHI Masaya <ta2gch@gmail.com>
2023-12-22 16:51:00 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tidy Tabs
+
+Tidy Tabs is a browser extension that keeps your tabs tidy.
+It allows you to automatically sort tabs based on their domain as follows:
+
+- It compares components (tld, top-level domain, subdomain, subsubdomain, etc.) lexicographically.
+  (e.g. com is smaller than org, example1 is smaller than example2)
+- It compares hostnames reverse lexicographically.
+  (e.g. example1.com is smaller than example2.org because com is smaller than org)
+- If one hostname contains another, the shorter one is smaller.
+  (e.g. example2.org is smaller than www.example2.org because example2.org is shorter than www.example2.org)
+
+# License
+
+Tidy Tabs is licensed under the MIT license.
+
+# Copyright
+
+This project is created by TANIGUCHI Masaya.
---
+++ background.js
@@ -0,0 +1,77 @@
+/****************************************************************************************
+ * background.js is a part of Tidy Tabs
+ *
+ * The MIT License (MIT)
+ * (c) 2023 TANIGUCHI Masaya
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a 
+ *  copy of this software and associated documentation files (the "Software"), 
+ *  to deal in the Software without restriction, including without limitation 
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ *  and/or sell copies of the Software, and to permit persons to whom the 
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  DEALINGS IN THE SOFTWARE.
+ ****************************************************************************************/
+
+/**
+ * revlex is a reverse lexicographic comparison of two arrays of strings.
+ * @param {Array<string>} components1
+ * @param {Array<string>} components2
+ * @return {number} -1 if components1 < components2, 0 if components1 == components2, 1 if components1 > components2
+ **/
+function revlex(a, b, cmp) {
+  const minLen = Math.min(a.length, b.length)
+  const ra = a.slice().reverse()
+  const rb = b.slice().reverse()
+  for (let i = 0; i < minLen; i++) {
+    const c = cmp(ra[i], rb[i])
+    if (c != 0) {
+      return c
+    }
+  }
+  return a.length - b.length
+}
+
+/**
+ * strCmp is a string comparison function.
+ * @param {string} component1
+ * @param {string} component2
+ * @return {number} -1 if component1 < component2, 0 if component1 == component2, 1 if component1 > component2
+ **/
+function strCmp(a, b) {
+  return a < b ? -1 : a == b ? 0 : 1
+}
+
+/**
+ * urlCmp is a URL comparison function.
+ * @param {string} url1
+ * @param {string} url2
+ * @return {number} -1 if url1 < url2, 0 if url1 == url2, 1 if url1 > url2
+ **/
+function urlCmp(url1, url2) {
+  const hostname1 = new URL(url1).hostname
+  const hostname2 = new URL(url2).hostname
+  const components1 = hostname1.split('.')
+  const components2 = hostname2.split('.')
+  return revlex(components1, components2, strCmp)
+}
+
+// Sort tabs when a tab is updated.
+browser.tabs.onUpdated.addListener(() => {
+  browser.tabs.query({currentWindow: true}, async (tabs) => {
+    const sortedTabs = tabs.toSorted((a, b) => urlCmp(a.url, b.url));
+    for (let i = 0; i < sortedTabs.length; i++) {
+      await browser.tabs.move(sortedTabs[i].id, {index: i});
+    }
+  });
+});
